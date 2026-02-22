e19243bc9d79ae8e7b824793f99e23b0b7de2ccc
Aryan Parakh <Aryan.24BCS10336@sst.scaler.com>
2026-02-22 17:26:42 +0530

    Create SimplePrinter.java

---
+++ SOLID/Ex1/src/SimplePrinter.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SimplePrinter {
+
+    public void printInput(String raw) {
+        System.out.println("INPUT: " + raw);
+    }
+
+    public void printErrors(List<String> errors) {
+        System.out.println("ERROR: cannot register");
+        for (String e : errors)
+            System.out.println("- " + e);
+    }
+
+    public void printSuccess(StudentRecord rec, int count) {
+        System.out.println("OK: created student " + rec.id);
+        System.out.println("Saved. Total students: " + count);
+        System.out.println("CONFIRMATION:");
+        System.out.println(rec);
+    }
+}
